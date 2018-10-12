9d75c6a2e8cbaa23521cbc851d9b9655bfc565ae
Beth Damato <bethadamato@gmail.com>
2018-10-12 13:27:13 -0400

    makes last move of html to js file

--- instaclone.js
+++ instaclone.js
@@ -4,7 +4,7 @@ var MY_MODAL = "[data-myModal]";
 var IMG_01 = "[data-img01]";
 var CAPTION = "[data-caption]";
 
-var images = [
+var katImages = [
   "images/meerkat_mom.jpg",
   "images/meerkat_rocks.jpg",
   "images/meerkat_fam.jpg",
@@ -13,19 +13,34 @@ var images = [
   "images/meerkat_pose.jpg",
 ];
 
+var katAlt = [
+  "get outta my way, YOU!",
+  "dude, they can't see us",
+  "why are your ears dirty",
+  "Buuuuuuup!",
+  "...and we're SMI-LING!",
+  "THIS...is a meerkat"
+];
+
 var footWords = [
-  'ABOUT US',
-  'SUPPORT',
-  'BLOG',
-  'PRESS',
-  'API',
-  'JOBS',
-  'PRIVACY',
-  'TERMS',
-  'DIRECTORY',
-  'PROFILES',
-  'HASHTAGS',
-  'LANGUAGE'  
+  "ABOUT US",
+  "SUPPORT",
+  "BLOG",
+  "PRESS",
+  "API",
+  "JOBS",
+  "PRIVACY",
+  "TERMS",
+  "DIRECTORY",
+  "PROFILES",
+  "HASHTAGS",
+  "LANGUAGE"  
+];
+
+var headLinks = [
+  "images/compass.png",
+  "images/heart.png",
+  "images/person.png"
 ];
 
 var navItems = document.querySelectorAll(THUMB_IMG);
@@ -38,10 +53,129 @@ var span = document.getElementsByClassName("close")[0];
 
 function createHeader() { 
   //create header container
-  var footerContainer = document.createElement("div", 
-  {class: "footerCont"});
-  var footerMenu = document.createElement("ul",
-  {class: "footerMenu"});
+  var headerContainer = document.createElement("div");
+  headerContainer.classList.add("headerCont"); 
+  var headSpaceLeft = document.createElement("div");
+  headSpaceLeft.classList.add("headerSpaceLeft"); 
+  headerContainer.appendChild(headSpaceLeft);
+
+  //create left block of header and add to container
+  var headLeftA = document.createElement("a");
+  headLeftA.setAttribute("href", "#");
+  var headLeftImg = document.createElement("img");
+  headLeftImg.setAttribute("src", "images/islogo.png");
+  headLeftA.appendChild(headLeftImg);
+  headSpaceLeft.appendChild(headLeftA);
+
+  //create middle block of header and add to container
+  var headSpaceMiddle = document.createElement("div");
+  headSpaceMiddle.classList.add("headerSpaceMiddle"); 
+  var headMiddleInput1 = document.createElement("input");
+  headMiddleInput1.setAttribute("type", "text");
+  headMiddleInput1.setAttribute("class", "searchTerm");
+  headMiddleInput1.setAttribute("placeholder", "Search");
+  headSpaceMiddle.appendChild(headMiddleInput1);
+  var headMiddleInput2 = document.createElement("input");
+  headMiddleInput2.setAttribute("type", "submit");
+  headMiddleInput2.setAttribute("class", "searchButton");;
+  headSpaceMiddle.appendChild(headMiddleInput2);
+  headerContainer.appendChild(headSpaceMiddle);
+
+
+  //creade right block of header and add to container
+  var headSpaceRight = document.createElement("div");
+  headSpaceRight.classList.add("headerSpaceRight");
+  var headUlTag = document.createElement("ul");
+  headSpaceRight.appendChild(headUlTag);
+
+  for(var i = 0; i < headLinks.length; i++) {
+    var headLiTag = document.createElement("li");
+    var headATag = document.createElement("a");
+    headATag.setAttribute("href", "#");
+    var headImgs = document.createElement("img");
+    headImgs.setAttribute("src", headLinks[i]);
+    headATag.appendChild(headImgs);
+    headLiTag.appendChild(headATag);
+    headUlTag.appendChild(headLiTag);
+  }
+
+  headerContainer.appendChild(headSpaceRight);
+  document.body.appendChild(headerContainer);
+}
+
+  function createMainImgContainer() {
+    var mainImgCont = document.createElement("div");
+    mainImgCont.classList.add("mainImageCont"); 
+    var lgImgCont = document.createElement("div");
+    lgImgCont.classList.add("lgImageCont");
+    mainImgCont.appendChild(lgImgCont);
+
+    //create large image container holding main image
+    var lgImgContA = document.createElement("a");
+    lgImgContA.setAttribute("href", "#");
+    lgImgCont.appendChild(lgImgContA);
+
+    var lgImgContImg = document.createElement("img");
+    lgImgContImg.setAttribute(MAIN_IMG, "");
+    lgImgContImg.setAttribute("src", "images/meerkat_mom.jpg");
+    lgImgContImg.setAttribute("alt", "get outta my way, YOU!");
+    lgImgCont.appendChild(lgImgContImg);
+
+    var modal = document.createElement("div");
+    modal.classList.add("modal");
+    modal.setAttribute("[data-myModal]", "");
+    var modalSpan = document.createElement("span");
+    modalSpan.classList.add("close");
+    modalSpan.innerHTML("X");
+    var modalImg = document.createElement("img");
+    modalImg.classList.add("modalContent"); 
+    modalImg.setAttribute("[data-img01]", "");
+    var modalCaption = document.createElement("div");
+    modalCaption.classList.add("caption");
+    modalCaption.setAttribute("[data-caption]", "");
+    modal.appendChild(modalCaption);
+    modal.appendChild(modalImg);
+    modal.appendChild(modalSpan);
+
+    mainImgCont.appendChild(modal);
+
+
+    //create the central thumbnail container
+    var thumbnailContainer = document.createElement("div");
+    thumbnailContainer.classList.add("thumbCont");
+    mainImgCont.appendChild(thumbnailContainer);
+
+    //cycle over each nav item
+    for(var i = 0; i < katImages.length; i++) {
+      var thumbDiv = document.createElement("div");
+      thumbDiv.classList.add("thumbItem");
+
+      var thumbA = document.createElement("a");
+      thumbA.setAttribute("[data-indiv-img]", "");
+      thumbA.setAttribute("href", katImages[i]);
+      thumbDiv.appendChild(thumbA);
+
+      var thumbImg = document.createElement("img");
+      thumbImg.setAttribute("src", katImages[i]);
+    
+      for (var i = 0; i < katAlt.length; i++) {
+        thumbImg.setAttribute("alt", katAlt[i]);
+      }
+
+      thumbA.appendChild(thumbImg);
+    }
+
+    document.body.appendChild(mainImgCont);
+  }
+  
+
+
+function createFooter() { 
+  //create footer container
+  var footerContainer = document.createElement("div");
+  footerContainer.classList.add("footerCont");
+  var footerMenu = document.createElement("ul");
+  footerMenu.classList.add("footerMenu");
   footerContainer.appendChild(footerMenu);
 
   //cycle over each nav item
@@ -60,12 +194,14 @@ function createHeader() {
     footerMenu.appendChild(liTag);
   }
 
-  function appendFooterToBody(footdiv) {
-    body.appendChild(footerContainer);
-  }
-
+    document.body.appendChild(footerContainer);
 }
 
+
+function main() {
+  createHeader();
+  createFooter();
+  createMainImgContainer();
 
   navItems.forEach(function (nav) {
     nav.addEventListener("click", function (event) {
@@ -74,10 +210,10 @@ function createHeader() {
 
       var imageInside = nav.querySelector("img");
       imgTarget.setAttribute("alt", imageInside.getAttribute("alt"))
-    })
+    });
   });
 
-
+  //the modal
   imgTarget.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
@@ -87,46 +223,5 @@ function createHeader() {
   span.onclick = function() { 
     modal.style.display = "none";
   }
-  
-
-
-function createFooter() { 
-  //create footer container
-  var footerContainer = document.createElement("div", 
-  {class: "footerCont"});
-  var footerMenu = document.createElement("ul",
-  {class: "footerMenu"});
-  footerContainer.appendChild(footerMenu);
-
-  //cycle over each nav item
-  for(var i = 0; i < footWords.length; i++) {
-
-    //create fresh list item and anchor
-    var liTag = document.createElement("li");
-    var aTag = document.createElement("a");
-
-    //set properties on anchor
-    aTag.setAttribute("href", "#");
-    aTag.innerHTML = footWords[i];
-
-    //add anchor to list item, and list item to list
-    liTag.appendChild(aTag);
-    footerMenu.appendChild(liTag);
-  }
-
-  // Add list to body (or anywhere else)
-  window.onload = function () {
-    document.body.appendChild(footerContainer);
-  }
-
 }
-
-
-// function main() {
-//   createHeader();
-//   createFooter();
-//   createMainImgContainer();
-//   addTheModal();
-// }
-
-// main();
+main();
